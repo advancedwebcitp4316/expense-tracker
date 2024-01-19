fa324c960112f845e36e1888607ce50d752bd7fe
advancedwebcitp4316@gmail.com <advancedwebcitp4316@gmail.com>
2024-01-19 07:25:40 -0600

    Adding Transactions

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@
   import Header from './components/Header.vue'
   import Balance from './components/Balance.vue'
   import IncomeExpenses from './components/IncomeExpenses.vue';
-
-  //const transactions = ref([])
-  const transactions = ref([
-    {id: 1, text: 'Flowers', amount: -19.99},
-    {id: 2, text: 'Salary', amount: 299.99},
-    {id: 3, text: 'Book', amount: -25.99},
-    {id: 4, text: 'Camera', amount: 250},
-  ])
+  import AddTransaction from './components/AddTransaction.vue';
+  const transactions = ref([])
+  // const transactions = ref([
+  //   {id: 1, text: 'Flowers', amount: -19.99},
+  //   {id: 2, text: 'Salary', amount: 299.99},
+  //   {id: 3, text: 'Book', amount: -25.99},
+  //   {id: 4, text: 'Camera', amount: 250},
+  // ])
 
   //get total 
   const total = computed(() => {
@@ -40,7 +40,12 @@
     .toFixed(2)
   })
 
-  
+  const handleTransactionSubmitted = (transactionData) => {
+    transactions.value.push({
+      text: transactionData.text,
+      amount: transactionData.amount,
+    })
+  }
 </script>
 
 <template>
@@ -48,5 +53,7 @@
   <div class="container">
     <Balance :total="+total"></Balance>
     <IncomeExpenses :income="+income" :expense="+expense"></IncomeExpenses>
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted"></AddTransaction>
+    <!-- {{ transactions }} -->
   </div>
 </template>
